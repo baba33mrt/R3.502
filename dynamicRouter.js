a42a65be80d3711fb8ca9e2a8bd1367cdca75960
baba33mrt <baptiste18032004@gmail.om>
2024-09-08 16:49:37 +0200

    optimisation, retrait des var → const/let

--- dynamicRouter.js
+++ dynamicRouter.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var router = express.Router();
-var appContext;
-var url = require("url");
-var fs = require('fs');
+const express = require("express");
+const router = express.Router();
+let appContext;
+// const url = require("url");
+const fs = require('fs');
 
 function dynamicRouter(app) {
     //-- Context applicatif
@@ -13,16 +13,16 @@ function dynamicRouter(app) {
     appContext.use(router);
 }
 
-/* Fonction qui permet d'aguiller les requêtes HTTP 
+/* Fonction qui permet d'aguiller les requêtes HTTP
 vers le bon contrôleur en fonction de l'action du pathname  */
 function manageAction(req, res, next) {
     req.message = {};
-    var path = req.originalUrl; // Le pathname après le port 3040 dans l'URL.
+    let path = req.originalUrl; // Le pathname après le port 3040 dans l'URL.
     if (path.includes("?")) {
         path = path.split('?')[0];
         if (path.split('/').length > 0) path = '/' + path.split('/')[1]
     } else if (path.split('/').length > 0) path = '/' + path.split('/')[1]
-    var type = req.method;
+    const type = req.method;
     // On défini la clé de l'annuaire config_actions.json dans une variable "action"
     req.message.action = type + path;
 
@@ -33,7 +33,7 @@ function manageAction(req, res, next) {
     /** AJOUT DE L'ENSEMBLE DES PARAMETRES DU FICHIERS "CONFIG_ACTIONS.JSON" DANS LE MESSAGE ASSOCIE A REQ */
     /***************************************************************************************************** */
     /* Boucle de récupération des paramètres de l'action du fichier config_actions.json */
-    for (param in global.actions_json[req.message.action]) {
+    for (let param in global.actions_json[req.message.action]) {
         req.message[param] = (global.actions_json[req.message.action])[param];
     }
     console.log('req.message dans dynamicRouteur : ', req.message);
@@ -41,11 +41,12 @@ function manageAction(req, res, next) {
     // Récupération des noms de champs pour un upload de fichiers passé dans le config_actions.json
     if (req.message.fieldUpload) global.fieldUpload = req.message.fieldUpload;
 
-    // (...) Il est donc possible d'ajouter des variables à l'annuaire config_actions.json 
-    // en fonction des besoins du développeur, elles seront automatiquemnt récupérées dans 
+    // (...) Il est donc possible d'ajouter des variables à l'annuaire config_actions.json
+    // en fonction des besoins du développeur, elles seront automatiquemnt récupérées dans
     // l'objet "req.message" dans les contrôleurs (routes/).
 
     // Si l'action n'est pas définie dans l'annuaire, on log l'erreur
+    let instanceModule;
     if (typeof global.actions_json[req.message.action] == 'undefined') {
         console.log("Erreur: Pas d'action dans l'annuaire config_actions.json : " + path);
         next();
